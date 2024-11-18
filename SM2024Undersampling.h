4764cf7978004d9d83a23e70a5e205dcd9a34637
Mateusz <twonightdragons@gmail.com>
2024-11-18 11:29:43 +0100

    Added corresponding heading files to SM2024Undersampling.h

--- SM2024Undersampling.h
+++ SM2024Undersampling.h
@@ -1,6 +1,14 @@
 #ifndef SM2024UNDERSAMPLING_H_INCLUDED
 #define SM2024UNDERSAMPLING_H_INCLUDED
 
-void calculateSubFilter();
+void podprobkowanieYIQ(int dx, int dy);
+
+void podprobkowanieYCbCr(int dx, int dy);
+
+void podprobkowanieYUV(int dx, int dy);
+
+void podprobkowanieHSL(int dx, int dy);
+
+
 
 #endif // SM2024UNDERSAMPLING_H_INCLUDED
